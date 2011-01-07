523ea437184ee17a4e4a10561276194cd21f4c26
Paul Coleman <pcoleman@new-host-2.home>
2011-01-07 10:03:07 -0500

    Cleaned up the code a lot and made it work for new registrations as well.

--- coreo/ucore/signals.py
+++ coreo/ucore/signals.py
@@ -1,7 +1,6 @@
 import datetime
 
 from django.core.mail import send_mail
-
 from coreo.ucore.models import CoreUser, SearchLog, Trophy, TrophyCase, Tag
 
 
@@ -9,10 +8,15 @@ def singular_check(user, search_tag_name):
   if (SearchLog.objects.filter(user=user, search_tags=Tag.objects.filter(name__startswith=search_tag_name)).count() > 4):
     trophyObj = Trophy.objects.get(tag=search_tag_name)
     if (trophyObj and TrophyCase.objects.filter(user=user, trophy=trophyObj).count() == 0):
-      custom_message = 'Congratulations %s, you have won a trophy (%s)' % (user.first_name, trophyObj.name)
-      send_mail(custom_message, 'Testing e-mail', 'trophy@layedintel.com', [user.email], fail_silently=False)
       t = TrophyCase(user=user, trophy=trophyObj, date_earned=datetime.datetime.now())
       t.save()
+
+
+def send_trophy_email(sender, instance, **kwargs):
+  user = CoreUser.objects.get(username=instance.user.username)
+  trophyObj = Trophy.objects.get(name=instance.trophy.name)
+  custom_message = 'Congratulations %s, you have won a trophy (%s)' % (user.first_name, trophyObj.name)
+  send_mail(custom_message, 'Testing e-mail', 'trophy@layedintel.com', [user.email], fail_silently=False)
 
 
 def check_for_trophy(sender, instance, **kwargs):
