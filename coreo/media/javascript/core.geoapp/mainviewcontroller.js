408c4223d04f51d4a8822b977dc372df0fbbfdb6
jashot7 <jashot7@gmail.com>
2011-02-10 23:50:30 -0500

    Added stub for the main view controller in new class structure.

---
+++ coreo/media/javascript/core.geoapp/mainviewcontroller.js
@@ -0,0 +1,64 @@
+/**
+ * Class: MainViewController
+ * 
+ * Controller for main view in the core geo app. ITS A STUB!
+ * 
+ * Namespace:
+ *	core.geoapp
+ *
+ * Dependencies:
+ *	- Many, many dependencies... filled in soon.
+ * Properties:
+ * 
+ *    
+ *         
+ * Methods:
+ * 
+ */
+
+if (!window.core)
+	window.core = {};
+if (!window.core.geoapp)
+	window.core.geo = {};
+
+(function($, ns) {
+	
+
+
+	/**
+	 * Constructor: MainViewController
+	 * Initializes the object 
+	 */
+	var MainViewController = function() {
+		
+	};
+
+	MainViewController.prototype = {
+		/**
+		 * Function: initialize
+		 * Returns:
+		 * 	This is a stub..
+		 * See Also: <thisIsAStubFunction>
+		 */			
+		initialize: function() {
+		},
+		
+		/**
+		 * Function: thisIsAStub
+		 * 
+		 * Returns: 
+		 * 	The KML as a hierarchical DOM object. NOT.
+		 * See Also: <initialize>
+		 */
+		thisIsAStub: function() {
+		}
+		
+	};
+
+	// Add MainViewController class to namespace.
+	ns.MainViewController = MainViewController;
+	
+	
+	
+})(jQuery, window.core.geoapp);
+
