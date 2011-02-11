f8d40d38aa9b439ee964d9f37ad138c2416395a5
jashot7 <jashot7@gmail.com>
2011-02-10 23:56:54 -0500

    .gitignore now ignores swp files from macvim. Also, gmaps controller stub has been added to hierarchy.

---
+++ coreo/media/javascript/core.gmaps/gmapscontroller.js
@@ -0,0 +1,48 @@
+/**
+ * Class: GmapsController
+ * 
+ * This is a stub currently.
+ *
+ * Namespace: 
+ *	core.gmaps
+ * 
+ * Dependencies:
+ *	- Will have many...
+ * 
+ */
+if (!window.core)
+	window.core = {};
+if (!window.core.gearth)
+	window.core.gmaps = {};
+
+(function(ns) {
+	
+	var GmapsController = function(gm) {
+		this.gm = gm;
+	};
+	GmaosController.prototype = {
+
+		add: function(geoData) {
+
+		},
+		
+		show: function(node) {
+			
+		},
+		
+		hide: function(node) {
+			
+		},
+		
+		showNodeInfo: function(node) {
+			
+		},
+		
+		flyToNode: function(node) {
+			
+		}
+		
+	};
+	ns.GmapsController = GmapsController;
+	
+})(window.core.gmaps);
