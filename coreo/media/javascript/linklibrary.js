b77ef307774a63694cba2a66186168055c23490c
Paul Coleman <pcoleman@Paul-Colemans-MacBook-Pro.local>
2011-02-11 08:17:37 -0500

    Got the multiple tag auto-complete portion working.

--- coreo/media/javascript/linklibrary.js
+++ coreo/media/javascript/linklibrary.js
@@ -1,10 +1,62 @@
       var totalTable = [];
       var grid;
+      
+      function split( val ) {
+         return val.split( /,\s*/ );
+      }
+      
+      function extractLast( term ) {
+         return split( term ).pop();
+      }
+
       $(document).ready(function()
       {
         document.getElementById('question').style.display='none';
         document.getElementById('myGrid').style.display='none';
+        $("input#tagname").bind( "keydown", function( event ) {
+              if ( event.keyCode === $.ui.keyCode.TAB &&
+                $( this ).data( "autocomplete" ).menu.active ) {
+                      event.preventDefault();
+                }
+        })
+        .autocomplete({
+               source: function(req, add){ 
+                $.getJSON("../get-tags/", 
+                  req, function(data) {  
+                    var suggestions = [];  
+                    $.each(data, function(i, val){
+                      suggestions.push(val.fields.name);  
+                   });
+                   
+                add(suggestions);  
+             })
+             }, 
+             search: function() {
+					    // custom minLength
+					    var term = extractLast( this.value );
+					    if ( term.length < 2 ) {
+					    	return false;
+				    	}
+			     	},
+				    focus: function() {
+				   	// prevent value inserted on focus
+				    	return false;
+				    },
+				    select: function( event, ui ) {
+					  var terms = split( this.value );
+					  // remove the current input
+					  terms.pop();
+					  // add the selected item
+					  terms.push( ui.item.value );
+					  // add placeholder to get the comma-and-space at the end
+					  terms.push( "" );
+					  this.value = terms.join( ", " );
+					  return false;
+				}
+		 	});
+          
       });
+
       function searchLinks(term)
        {
          // alert('Got to searchLinks and passed in : ' + term);
@@ -31,15 +83,15 @@
           columns.push({ id: "desc", name: "desc", field: "description", width:300,
                editor: TextCellEditor
            });
-          columns.push({ id: "url", name: "url", field: "urlfield", width:500,
-               editor: TextCellEditor
-           });
+        //  columns.push({ id: "url", name: "url", field: "urlfield", width:500,
+        //       editor: TextCellEditor
+        //   });
            for (var i=0; i < jsonstuff.length; i++)
            {
                var d = (totalTable[i] = {}); 
                d["name"] = jsonstuff[i].fields.name;
                d["description"] = jsonstuff[i].fields.desc;
-               d["urlfield"] = jsonstuff[i].fields.url;
+              //  d["urlfield"] = jsonstuff[i].fields.url;
                d["pk"] = jsonstuff[i].pk;
           }
           var options = { editable: true, enableCellNavigation: true, asyncEditorLoading: false, autoEdit: false };
@@ -90,7 +142,8 @@
                }
                var library_name = document.getElementById('q1').value;
                var library_desc = document.getElementById('q2').value;
-               $.post("../create-library/", { name: library_name, desc: library_desc, links: row_parameter});
+               var tagparameter = document.getElementById('tagname').value;
+               $.post("../create-library/", { name: library_name, desc: library_desc, tags: tagparameter, links: row_parameter});
                //  alert("you entered: " + document.getElementById('q1').value);
                $("#dialog").dialog("close");
          } 
