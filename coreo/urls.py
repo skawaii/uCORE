b9b45202e4bdf864be70e7ca97987b7ec4981c57
Paul Coleman <pcoleman@new-host-2.home>
2011-02-14 12:53:56 -0500

    Was trying not to commit this, but merging can't happen unless I stash.

--- coreo/urls.py
+++ coreo/urls.py
@@ -8,10 +8,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':  '/Users/skawaii/sandbox/work/ucore/coreo/media/', 'show_indexes':True}),
+    #(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':  '/Users/skawaii/sandbox/work/ucore/coreo/media/', 'show_indexes':True}),
     #(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':  'C:/dev/django-1.2.3/ucore/coreo/media/', 'show_indexes':True}),
     #(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':  '/Users/pboone/code/ucore/coreo/media/', 'show_indexes':True}),
-    #(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':  '/Users/pcoleman/uCORE/coreo/media', 'show_indexes':True}), 
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':  '/Users/pcoleman/uCORE/coreo/media', 'show_indexes':True}), 
     # (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':   '/Users/jashot7/Code/uCORE/uCORE/coreo/media', 'show_indexes':True}),  
     # Example:
     # (r'^coreo/', include('coreo.foo.urls')),
