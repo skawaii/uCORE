d9eca7940f605a4a591951d04296bb8fb015001d
Paul Boone <paul.t.boone@gmail.com>
2011-02-28 16:29:45 -0500

    JS documentation

--- coreo/media/javascript/core.util/assert.js
+++ coreo/media/javascript/core.util/assert.js
@@ -4,12 +4,6 @@
  * Assertion functions. Each function tests evaluates some condition and 
  * raises an exception if the condition is false.
  * 
- * Functions:
- *  - isTrue - Asserts a condition is true.
- *  - notNull - Asserts an object is not null or undefined.
- *  - same - Asserts two objects are identical (tests equality with ===).
- *  - type - Asserts an object is of a certain type.
- *  
  * Namespace:
  *  core.util
  *  
@@ -28,11 +22,30 @@ if (!window.core.util)
 				: val;
 	};
 	var Assert = {
+		/**
+		 * Function: isTrue
+		 * 
+		 * Raises an exception if a condition is false.
+		 * 
+		 * Parameters:
+		 *   condition - Boolean.
+		 *   error - String or Error. exception to raise if condition is false.
+		 */
 		isTrue: function(condition, error) {
 			if (condition == false)
 				throw defaultIfNull(error, "Condition is false");
 		},
 		
+		/**
+		 * Function: hasMember
+		 * 
+		 * Raises an exception if an object doesn't have a named member.
+		 * 
+		 * Parameters:
+		 *   o - Object.
+		 *   memberName - String. Name of member.
+		 *   error - String or Error.
+		 */
 		hasMember: function(o, memberName, error) {
 			Assert.isTrue(o !== null && o !== undefined 
 					&& memberName !== null && memberName !== undefined 
@@ -41,6 +54,16 @@ if (!window.core.util)
 							+ memberName + "'"));
 		},
 		
+		/**
+		 * Function: hasMember
+		 * 
+		 * Raises an exception if an object doesn't have a named function.
+		 * 
+		 * Parameters:
+		 *   o - Object.
+		 *   functionName - String.
+		 *   error - String or Error.
+		 */
 		hasFunction: function(o, functionName, error) {
 			error = defaultIfNull(error, 
 					"Object does not contain function '" + functionName + "'");
@@ -48,16 +71,45 @@ if (!window.core.util)
 			Assert.same(typeof o[functionName], "function", error);
 		},
 		
+		/**
+		 * Function: notNull
+		 * 
+		 * Raises an exception if an object is null or undefined.
+		 * 
+		 * Parameters:
+		 *   o - Object.
+		 *   error - String or Error.
+		 */
 		notNull: function(o, error) {
 			Assert.isTrue(o !== null && o !== undefined, 
 					defaultIfNull(error, "Object is null"));
 		},
 		
+		/**
+		 * Function: same
+		 * 
+		 * Raises an exception if two objects are not identical.
+		 * 
+		 * Parameters:
+		 *   a - Object.
+		 *   b - Object.
+		 *   error - String or Error.
+		 */
 		same: function(a, b, error) {
 			Assert.isTrue(a === b, 
 					defaultIfNull(error, "Objects are not the same"));
 		},
 		
+		/**
+		 * Function: type
+		 * 
+		 * Raises an exception if an object is not of an expected datatype.
+		 * 
+		 * Parameters:
+		 *   o - Object.
+		 *   expectedType - String.
+		 *   error - String or Error.
+		 */
 		type: function(o, expectedType, error) {
 			var actualType = typeof o;
 			Assert.same(actualType, expectedType,
@@ -65,9 +117,7 @@ if (!window.core.util)
 							new TypeError("Expected object of type '" 
 									+ expectedType + "', not '" 
 									+ actualType + "'")));
-		},
-		
-		
+		}
 		
 	};
 	ns.Assert = Assert;
